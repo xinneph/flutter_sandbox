138ccfa3311b309107c99ac272356c026706758d
piotr <xinneph@gmail.com>
2022-02-16 11:35:27 +0100

    Composing messages.

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,70 @@
 import 'package:flutter/material.dart';
 
-main() => runApp(
-  MaterialApp(
-    title: 'FriendlyChat',
-    home: Scaffold(
+main() => runApp(const FriendlyChatApp());
+
+class FriendlyChatApp extends StatelessWidget {
+  const FriendlyChatApp({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      title: 'FriendlyChat',
+      home: ChatScreen(),
+    );
+  }
+}
+
+class ChatScreen extends StatefulWidget {
+  const ChatScreen({Key? key}) : super(key: key);
+
+  @override
+  State<ChatScreen> createState() => _ChatScreenState();
+}
+
+class _ChatScreenState extends State<ChatScreen> {
+  final _textController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
       appBar: AppBar(
-        title: const Text('Friendly Chat'),
+        title: const Text('Friendly chat'),
       ),
-    ),
-  )
-);
+      body: _buildTextComposer(),
+    );
+  }
+
+  Widget _buildTextComposer() {
+    return Container(
+      margin: const EdgeInsets.symmetric(horizontal: 8.0),
+      child: Row(
+        children: [
+          Flexible(
+            child: TextField(
+              controller: _textController,
+              onSubmitted: _handleSubmitted,
+              decoration:
+                  const InputDecoration.collapsed(hintText: 'Send a message'),
+            ),
+          ),
+          IconTheme(
+            data: IconThemeData(color: Theme.of(context).colorScheme.secondary),
+            child: Container(
+              margin: const EdgeInsets.symmetric(horizontal: 4.0),
+              child: IconButton(
+                onPressed: () => _handleSubmitted(_textController.text),
+                icon: const Icon(Icons.send),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  void _handleSubmitted(String string) {
+    _textController.clear();
+  }
+}
